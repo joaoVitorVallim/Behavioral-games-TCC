ea3d08b25e406d0d472b974a97f91fc791690d06
Joao Vitor <joaovitor.vallim@hotmail.com>
2026-02-05 18:19:12 -0300

    [ADD] swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       transform: true,        // converte tipos automaticamente
     }),
   );
+
+    const config = new DocumentBuilder()
+    .setTitle('Minha API')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
