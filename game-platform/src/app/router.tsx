583937e5b5bf2976a9e70034e35c4bf91db4a94d
ltbreno1 <ltbreno1@gmail.com>
2026-05-14 19:40:36 -0300

    feat: remove all code related to game and changing the icon tab

--- game-platform/src/app/router.tsx
+++ game-platform/src/app/router.tsx
@@ -3,7 +3,6 @@ import { LoginPage } from '../modules/auth/pages/LoginPage'
 import { SessionsPage } from '../modules/game-session/pages/SessionsPage'
 import { CreateSessionPage } from '../modules/game-session/pages/CreateSessionPage'
 import { ReportsPage } from '../modules/reports/pages/ReportsPage'
-import { PhaserGamePage } from '../modules/phaser-game/pages/PhaserGamePage'
 import { ProtectedRoute } from './components/ProtectedRoute'
 
 export const router = createBrowserRouter([
@@ -30,9 +29,5 @@ export const router = createBrowserRouter([
         <ReportsPage />
       </ProtectedRoute>
     ),
-  },
-  {
-    path: '/game',
-    element: <PhaserGamePage />,
   }
 ])
